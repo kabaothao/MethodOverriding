b7c4f3a05b251763844f2e14b814171b8439fb36
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-07 15:36:13 -0600

    refactor codes

--- MethodOverriding/Position.cs
+++ MethodOverriding/Position.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MethodOverriding
 {
-    internal class Position
+    public class Position
     {
     }
 }
--- MethodOverriding/Program.cs
+++ MethodOverriding/Program.cs
@@ -3,9 +3,9 @@
 
 using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace MethodOverriding // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
@@ -19,7 +19,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         }
     }
 
-
+}
 
 
 
