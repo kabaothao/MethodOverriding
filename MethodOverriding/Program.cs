6c598a30a594bf4ed3be0729d33bb2e8ce16e94f
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-07 14:57:29 -0600

    Add project files.

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+
+
+
+/*
+ Overloading is having a method with the same name but different signatures overwriting on the other
+
+side is about changing the implementation of an inherited method.
+
+ */
